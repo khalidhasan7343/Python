55c624448b43daf9f9c0cc5ddc5633a0512b4bc5
Khalid Hasan <mdkhalidhasan7343@gmail.com>
2026-05-19 02:01:28 +0600

    second

---
+++ for_loop.py
@@ -0,0 +1,19 @@
+# 1 - 1000
+a = [1,2,3,4]
+
+b = "Rahat"
+
+for item in a: # 1 2 3 4 
+    print(item)
+
+for char in b: # R a h a t
+    print(char)
+
+print('e' in "Rahat")
+
+for i in range(1, 1001):
+    print(i)
+
+for i in range(101):
+    print(f"Nasa is hacking... {i}%")
+print("Nasa has hacked your computer...")
--- range_python.py
+++ range_python.py
@@ -2,6 +2,10 @@ a =list(range(10))
 b = tuple(range(10))
 c = list(range(1, 10 , 2))
 # range(start, stop, step)
+d = list(range(1, 100 , 5))
+e = list(range(10, 0 , -1))
 print(a) # [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 print(b) # (0, 1, 2, 3, 4, 5, 6, 7, 8, 9)
 print(c) # [1, 3, 5, 7, 9]
+print(d) # [1, 6, 11, 16, 21, 26, 31, 36, 41, 46, 51, 56, 61, 66, 71, 76, 81, 86, 91, 96]
+print(e) # [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
